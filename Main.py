6f58edb1773c8ef03aeff57b111819e21b6a8475
Viktoria <viki88830@gmail.com>
2023-09-19 17:48:41 +0200

    Created the basic script that organises files

---
+++ Main.py
@@ -0,0 +1,58 @@
+import os
+from collections import defaultdict
+import shutil
+import time
+dir = "/home/viktoria/Downloads"
+def organise_folder(dir):
+    files = os.listdir(dir)
+    # I will not show hidden files
+    # Hidden files are those that start with a dot (.)
+    visible_files = [file for file in files if not file.startswith('.')]
+    names_of_folders = {
+        "pdf" : "Pdf Files",
+        "jpg" : "Images",
+        "png" : "Images",
+        "mp3" : "Music",
+        "zip" : "Zip Files"
+    }
+    files_types = defaultdict(list)
+    valid_expressions = set(names_of_folders.keys())
+    # Adding programming languages stuff
+    for l in ["cpp", "py", "c", "rkt","java"]:
+        names_of_folders[l] = "Code files"
+
+    # Maping folder names to corrseponding files
+    for file in visible_files:
+        type = file.split(".")[-1]
+        if type in valid_expressions:
+            files_types[names_of_folders[type]].append(file)
+
+    # Creating directories and moving files;
+    for k, v in files_types.items():
+        path = os.path.join(dir,k)
+        if not os.path.exists(path):
+            os.mkdir(path)
+        for file in v:
+            try:
+                destination_path = os.path.join(path,file)
+                if os.path.exists(destination_path):
+                    base_name, ext = os.path.splitext(destination_path)
+                    i = 1
+                    while os.path.exists(destination_path):
+                        destination_path = f"{base_name}_{i}{ext}"
+                        i += 1
+                source_path = os.path.join(dir,file)
+                shutil.move(source_path,path)
+            except FileExistsError:
+                print(f"'{file}' already exists.")
+            except shutil.Error as e:
+                print(f"An error occurred: {e}")
+
+while True:
+    organise_folder(dir)
+    # Organizes folder every 3 hours
+    time.sleep(1)
+
+
+
+
